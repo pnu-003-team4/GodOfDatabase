f387b021cd7e5da5366614c02f329e650acd54c7
snowcrab <48473962+kmnj951@users.noreply.github.com>
2019-03-19 20:00:52 +0900

    #GODD-2

--- src/god.java
+++ src/god.java
@@ -8,6 +8,6 @@ public class god {
 		System.out.println("hello");
 		System.out.println("hello");
 		System.out.println("hello");
-		System.out.println("hello");
+		System.out.println("hel");
 	}
 }
