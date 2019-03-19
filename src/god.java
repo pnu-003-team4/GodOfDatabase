3eff48ee21a40b4bcf26e63041f9a5c9cfdd3e6b
임종성 <leesenggi@gmail.com>
2019-03-19 19:40:25 +0900

    #GODD-2

--- src/god.java
+++ src/god.java
@@ -3,5 +3,13 @@ public class god {
 	public static void main(String args[]) { 
 		System.out.println("hello");
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 }
