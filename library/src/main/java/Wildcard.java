3ea330b4a11668b7a65a1fec5090bd2c4128335c
hiena95 <gg2059@naver.com>
2019-04-02 20:11:05 +0900

    test

--- library/src/main/java/Wildcard.java
+++ library/src/main/java/Wildcard.java
@@ -4,7 +4,7 @@ public class Wildcard {
     public static String[] extractWildcard(List minjae, String path) {
         /*
          * TODO: path를 받았을 때(ex, "/Korea/Busan/University/*"), 연관된 path를 찾아내어 배열로 Return
-         * TODO: 민재가 만드는 리스트(path와 index를 매칭시키는)를 가져와서 확인한다.
+         * TODO: 민재가 만드는 리스트(path와 index를 매칭시키는)를 가져와서 확인한다....
          *
          * return: 와일드카드와 연관된 path
          */
