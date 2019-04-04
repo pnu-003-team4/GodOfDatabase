3d5130daf58f463d51df6e59ea6c510a406d3a29
mingtwo0226 <flovet0607@naver.com>
2019-04-04 18:07:56 +0900

    snappyDB 데이터 입력,삭제, 검색

--- app/src/main/java/com/pnu/godofdatabase/snappyDB_connection.java
+++ app/src/main/java/com/pnu/godofdatabase/snappyDB_connection.java
@@ -2,6 +2,11 @@ package com.pnu.godofdatabase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.snappydb.DB;
 import com.snappydb.DBFactory;
@@ -13,22 +18,83 @@ public class snappyDB_connection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         try {
-            DB snappydb = DBFactory.open(this); //create or open an existing database using the default name
+            final DB snappydb = DBFactory.open(this);
 
-            snappydb.put("name", "Jack Reacher");
-            snappydb.putInt("age", 42);
-            snappydb.putBoolean("single", true);
-            snappydb.put("books", new String[]{"One Shot", "Tripwire", "61 Hours"});
+            final EditText edit_put = (EditText)findViewById(R.id.edit_put);
+            final EditText edit_select = (EditText)findViewById(R.id.edit_select);
+            final EditText edit_del = (EditText)findViewById(R.id.edit_del);
 
-            String 	 name   =  snappydb.get("name");
-            int 	   age    =  snappydb.getInt("age");
-            boolean  single =  snappydb.getBoolean("single");
-            String[] books  =  snappydb.getArray("books", String.class);// get array of string
+            final TextView textView = (TextView)findViewById(R.id.output);
 
-            snappydb.close();
+            Button btn_put = (Button) findViewById(R.id.btn_put);
+            Button btn_select = (Button) findViewById(R.id.btn_select);
+            Button btn_del = (Button)findViewById(R.id.btn_del);
+
+            btn_put.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String input = edit_put.getText().toString();
+                    String key = null, value=null;
+                    int idx = input.indexOf(",");
+                    if(idx != -1) {
+                        key = input.substring(0, idx);
+                        value = input.substring(idx + 1);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"Usage: 'key', 'value' 입력",Toast.LENGTH_LONG).show();
+                        edit_put.setText(null);
+                        textView.setText(null);
+                    }
+
+                    try {
+                        snappydb.put(key, value);
+                        Toast.makeText(getApplicationContext(),"입력되었습니다",Toast.LENGTH_LONG).show();
+                        edit_put.setText(null);
+                    } catch (SnappydbException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(),"입력되지못했습니다",Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+            btn_del.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String key = edit_del.getText().toString();
+                    try {
+                        snappydb.del(key);
+                        Toast.makeText(getApplicationContext(),"삭제되었습니다",Toast.LENGTH_LONG).show();
+                        edit_del.setText(null);
+                    } catch (SnappydbException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(),"삭제되지못했습니다",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+
+            btn_select.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String output = edit_select.getText().toString();
+                    String value;
+                    edit_select.setText(null);
+                    try {
+                        value = snappydb.get(output);
+                        textView.setText(value);
+                    } catch (SnappydbException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(),"value값이 없습니다",Toast.LENGTH_LONG).show();
+                        edit_select.setText(null);
+                        textView.setText(null);
+                    }
+                }
+            });
 
         } catch (SnappydbException e) {
+            e.printStackTrace();
         }
     }
 
