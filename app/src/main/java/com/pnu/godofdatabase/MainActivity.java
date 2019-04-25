03eaeb04e51690606dedd920de04e9e034d670a6
SuhoSon <ngeol564@gmail.com>
2019-04-25 19:03:55 +0900

    MainActivity 수정

--- app/src/main/java/com/pnu/godofdatabase/MainActivity.java
+++ app/src/main/java/com/pnu/godofdatabase/MainActivity.java
@@ -1,9 +1,7 @@
 package com.pnu.godofdatabase;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +15,6 @@ import com.goddb.GoddbException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import android.view.View;
-import android.widget.Button;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -92,35 +86,5 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             return null;
         }
-
-        Button btn_GOD = (Button) findViewById(R.id.btn_GOD);
-        Button btn_sqlite = (Button) findViewById(R.id.btn_sqlite);
-        Button btn_snappy = (Button)findViewById(R.id.btn_snappy);
-        Button btn_cal = (Button)findViewById(R.id.btn_cal);
-
-
-        btn_GOD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, GOD_connection.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_snappy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, snappyDB_connection.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_sqlite.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, SQLite_connection.class);
-                startActivity(intent);
-            }
-        });
     }
 }
