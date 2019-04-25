99adaff05de2b54ae72f5f896b57d689d7a68ca4
SuhoSon <ngeol564@gmail.com>
2019-04-25 18:20:34 +0900

    GodOfDatabase Test Case

--- app/src/main/java/com/pnu/godofdatabase/MainActivity.java
+++ app/src/main/java/com/pnu/godofdatabase/MainActivity.java
@@ -2,40 +2,89 @@ package com.pnu.godofdatabase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.goddb.DB;
 import com.goddb.DBFactory;
 import com.goddb.GoddbException;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText inputPath;
+    EditText inputName;
+    EditText inputAge;
+    Button inputBtn;
+    Button outputBtn;
+    TextView resultText;
+    DB godDB; //create or open an existing database using the default name
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            DB godDB = DBFactory.open("DB"); //create or open an existing database using the default name
 
-            JSONObject student = new JSONObject();
+        inputPath = findViewById(R.id.inputPath);
+        inputName = findViewById(R.id.inputName);
+        inputAge = findViewById(R.id.inputAge);
+        inputBtn = findViewById(R.id.inputBtn);
+        outputBtn = findViewById(R.id.outputBtn);
+        resultText = findViewById(R.id.result);
 
-            try {
-                student.put("name", "JSON");
-                student.put("age", "23");
-            } catch (JSONException e1) {
-                e1.printStackTrace();
+        inputBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                JSONObject student = new JSONObject();
+
+                try {
+                    student.put("name", inputName.getText());
+                    student.put("age", inputAge.getText());
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+                putTest(String.valueOf(inputPath.getText()), student);
             }
+        });
 
-            godDB.put("/Korea/Busan/University/PNU", student);
-            godDB.get("/Korea/Busan/*/PNU", null);
+        outputBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                JSONArray temp = getTest(String.valueOf(inputPath.getText()));
 
-            godDB.del("Korea/Busan/University/PNU", "name==ParkJeongHwan");
+                if (temp != null) {
+                    resultText.setText(temp.toString());
+                } else {
+                    Log.d("get", "null.");
+                }
+            }
+        });
+    }
 
+    void putTest(String path, JSONObject obj) {
+        try {
+            godDB = DBFactory.open(this); //create or open an existing database using the default name
+            godDB.put(path, obj);
             godDB.close();
         } catch (GoddbException e) {
             e.printStackTrace();
+        }
+    }
+
+    JSONArray getTest(String path) {
+        try {
+            godDB = DBFactory.open(this); //create or open an existing database using the default name
+            JSONArray ret = godDB.get(path, null);
+            godDB.close();
+
+            return ret;
+        } catch (GoddbException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
