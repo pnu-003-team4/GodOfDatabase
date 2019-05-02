9ec1b434be38dd70576ab9c92f56a6c5be16f1a4
jeongjihong <jjh1731@naver.com>
2019-05-02 21:32:31 +0900

    -연산자가 적용되었을 때 <=, >= 의 기능 추가

--- library/src/main/java/com/goddb/internal/Condition.java
+++ library/src/main/java/com/goddb/internal/Condition.java
@@ -59,6 +59,16 @@ public class Condition {
                 idx = condition.indexOf("==");
                 leftOperand = condition.substring(0, idx);
                 rightOperand = condition.substring(idx+2);
+            } else if(condition.indexOf(">=") != -1) {
+                operator = "<";
+                idx = condition.indexOf("=");
+                leftOperand = condition.substring(0, idx-1);
+                rightOperand = condition.substring(idx+1);
+            } else if(condition.indexOf("<=") != -1) {
+                operator = ">";
+                idx = condition.indexOf("=");
+                leftOperand = condition.substring(0, idx-1);
+                rightOperand = condition.substring(idx+1);
             } else if (condition.indexOf(">") != -1) {
                 operator = "<=";
                 idx = condition.indexOf(">");
